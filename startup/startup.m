0e6ff0c46388ca9bc7961b409db7e4639db55550
agaier <adam.ga@gmail.com>
2016-05-16 16:12:15 +0200

    Default starting options (figure docking)

---
+++ startup/startup.m
@@ -0,0 +1,14 @@
+%% Start up options
+% Save this in your matlab path (type 'path' into the editor to see
+% possible locations) and these commands will be used whenever you start
+% matlab
+%
+% Author: Adam Gaier
+% Bonn-Rhein-Sieg University of Applied Sciences (BRSU)
+% email: adam.gaier@h-brs.de
+% May 2016; Last revision: 16-Ma-2016
+%
+%------------- BEGIN CODE --------------
+
+% Dock Figures
+set(0,'DefaultFigureWindowStyle','docked')
