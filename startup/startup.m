eb0b62099d748f447a494d0330604929f9b2e9c0
agaier <adam.ga@gmail.com>
2017-07-30 15:46:36 +0200

    updated startup to include matlabExtensions folder

--- startup/startup.m
+++ startup/startup.m
@@ -3,15 +3,15 @@
 % possible locations) and these commands will be used whenever you start
 % matlab
 %
-% Example: !cp startup.m /home/adam/Documents/MATLAB
+% Example: !cp ~/Code/matlabExtensions/startup/startup.m ~/Documents/MATLAB
 %
 % Author: Adam Gaier
-% Bonn-Rhein-Sieg University of Applied Sciences (BRSU)
+% Bonn-Rhein-Sieg University of Applied Sciences (HBRS)
 % email: adam.gaier@h-brs.de
-% May 2016; Last revision: 16-Ma-2016
+% May 2016; Last revision: 30-Jul-2017
 %
 %------------- BEGIN CODE --------------
 
 % Dock Figures
 set(0,'DefaultFigureWindowStyle','docked')
-addpath(genpath('/home/adam/Code/matlabExtensions'))
+addpath(genpath('~/Code/matlabExtensions/'))
