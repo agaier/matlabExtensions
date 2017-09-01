675bd09c38d421ff610acc91452266a9cfe530a5
agaier <adam.ga@gmail.com>
2017-09-01 12:47:53 +0200

    added author info

--- plotting/scatterLine.m
+++ plotting/scatterLine.m
@@ -1,6 +1,11 @@
 function [sh,lh] = scatterLine(xData,yData,varargin)
 %% Scatter Line
-% Plots points as a scatter plot with a smoothed line
+% Plots points as a scatter plot with a smoothed trend line
+%
+% Author: Adam Gaier
+% Bonn-Rhein-Sieg University of Applied Sciences (BRSU)
+% email: adam.gaier@h-brs.de
+% Jun 2017; Last revision: 01-Sep-2017
 
 if ~isempty(varargin)
     lh = plot(xData,smooth(yData,0.25,'rloess'),varargin{:});
