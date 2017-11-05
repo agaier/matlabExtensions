05fedbf3579bb86a578e954ed3cde2fd044e34a0
agaier <adam.gaier@h-brs.de>
2017-11-05 14:18:58 +0100

    histrogram by percentage of solutions

---
+++ plotting/histPercent.m
@@ -0,0 +1,11 @@
+function [handle] = histPercent(data, varargin)
+%% histPercent
+% Plots histogram with y-axis scaled to percent of total
+%
+% Author: Adam Gaier
+% Bonn-Rhein-Sieg University of Applied Sciences (BRSU)
+% email: adam.gaier@h-brs.de
+% Nov 2017; Last revision: 05-Nov-2017
+
+[freqCount, binValue] = hist(data, varargin{:});
+handle = bar(binValue, freqCount./length(data));
